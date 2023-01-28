40fafe44087f1a30f8be1f3ca16eb900fafebe37
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2023-01-28 23:20:24 +0530

    add sample pages
    Co-Authored-By: Kanishka Pasindu <109420850+kanishkapasi@users.noreply.github.com>

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container, Row, Col } from "reactstrap";
+
+const Contact = () => {
+  return (
+    <div>
+      Contact
+    </div>
+  )
+}
+
+export default Contact
